45acd68dea43e4f1fb51bccc0079e94b5c23f407
Abhishek Mishra <abhishekmishra24112004@gmail.com>
2026-05-06 16:52:25 +0530

    Update layout.tsx

--- app/layout.tsx
+++ app/layout.tsx
@@ -3,12 +3,8 @@ import { Geist, Geist_Mono } from 'next/font/google'
 import { Analytics } from '@vercel/analytics/next'
 import './globals.css'
 
-const _geist = Geist({ subsets: ["latin"] });
-const _geistMono = Geist_Mono({ subsets: ["latin"] });
-import Script from "next/script";
-
-
-
+const _geist = Geist({ subsets: ['latin'] })
+const _geistMono = Geist_Mono({ subsets: ['latin'] })
 
 export const metadata: Metadata = {
   title: 'LoanExpert - Loan & Eligibility Calculator',
@@ -28,13 +24,6 @@ export default function RootLayout({
   return (
     <html lang="en" suppressHydrationWarning>
       <body className="font-sans antialiased">
-      <Script
-          id="leadmind-tracker"
-          src="http://localhost:8000/public/sdk/leadmind-tracker.js"
-          data-api-key="lm_live_U3gJ-qz0GyrEKwRMswQiXS2xArTKJpJd"
-          data-api-host="http://localhost:8000"
-          strategy="afterInteractive"
-        />
         {children}
 
         <Analytics />
